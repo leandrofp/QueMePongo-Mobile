68345a005baa9c018e1374a8c7ac8ed3b9e595b0
leandrofp <leandro.fernandezp@gmail.com>
2018-10-14 19:58:32 -0300

    Agregado modulo de firebase 3.3 por problemas de compatibilidad en versiones posteriores

--- android/app/src/main/java/com/quemepongo/MainApplication.java
+++ android/app/src/main/java/com/quemepongo/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import org.pgsqlite.SQLitePluginPackage;
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import fr.snapp.imagebase64.RNImgToBase64Package;
 import io.codebakery.imagerotate.ImageRotatePackage;
 import org.reactnative.camera.RNCameraPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new RNImgToBase64Package(),
             new ImageRotatePackage(),
             new RNCameraPackage(),
